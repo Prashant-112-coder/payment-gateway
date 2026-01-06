8444cfdea46c22778344ce35a17aa3a84c7357a7
Prashant Ikkalaki <prashantikkalaki143@gmail.com>
2026-01-06 07:31:15 +0000

    Added Razor pay checkout

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function buyNow() {
   alert("Payment gateway will open here");
 }
+function buyNow() {
+  var options = {
+    key: "rzp_test_XXXXXXXXXX", // 🟡 your Razorpay TEST key here
+    amount: 9900, // ₹99 in paise
+    currency: "INR",
+    name: "Prashant Resume Store",
+    description: "Modern Resume Template",
+    handler: function (response) {
+      alert("Payment successful!\nPayment ID: " + response.razorpay_payment_id);
+    },
+    theme: {
+      color: "#2b7cff"
+    }
+  };
+  
+  var rzp = new Razorpay(options);
+  rzp.open();
+}
